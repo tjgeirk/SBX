75f451056dc32072a6777bebbf589c58ed179a27
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2022-12-31 08:07:09 -0800

    Update Stop_Hunt_McGee.py

--- Custom_Indicators/Stop_Hunt_McGee.py
+++ Custom_Indicators/Stop_Hunt_McGee.py
@@ -2,7 +2,7 @@
 from statistics import mean
 from pandas import DataFrame
 
-def KLE(data:DataFrame, window:int=7) -> DataFrame:
+def KLE(data:DataFrame, window:int=21) -> DataFrame:
     df = {'highs':[],'lows':[]}
     for x in range(1, window+1):
         df['highs'].append(data['high'].iloc[-x])
@@ -12,15 +12,4 @@ def KLE(data:DataFrame, window:int=7) -> DataFrame:
     support = (min(df)*support_pct)
     resist = (max(df)*resist_pct)
     return {'sup':support, 'res':resist}
-    
-book = lambda coin: exchange.fetch_order_book(coin)
-
-for i,v in enumerate(book()['asks'][0]):
-    if max(book()['asks'][1]) == book()['asks'][1][i]:
-        ask = book()['asks'][0][i]
-    if max(book()['bids'][1]) == book()['bids'][1][i]:
-        bid = book()['bids'][0][i]
-    return {'ask':ask,'bid':bid}
-        
-    
     
