6f089dc3bac5c50f072d0042be951c5956970672
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2023-01-01 16:21:22 -0800

    Update Stop_Hunt_McGee.py

--- Custom_Indicators/Stop_Hunt_McGee.py
+++ Custom_Indicators/Stop_Hunt_McGee.py
@@ -3,13 +3,11 @@ from statistics import mean
 from pandas import DataFrame
 
 def Stop_Hunt_McGee(data:DataFrame, window:int=21) -> DataFrame:
-    highs = []
-    lows = []
+    highs = lows = []
     for x in range(1, window+1):
-        highs.append(data('high').iloc[-window-x])
-        lows.append(data('low').iloc[-window-x])
-    resist_pct = max(highs)/mean(highs)
-    support_pct = min(lows)/mean(lows)
-    support = (min(lows)*support_pct)
-    resist = (max(highs)*resist_pct)
+        highs.append(data('high').iloc[-x])
+        lows.append(data('low').iloc[-x])
+    support = mode(lows)
+    resist =  mode(highs)
     return {'sup':support, 'res':resist}
+
