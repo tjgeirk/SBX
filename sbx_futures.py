b8990e34f9d6de0839a2254881292d325b694724
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2023-01-08 09:56:37 -0800

    Update sbx_futures.py

--- sbx_futures.py
+++ sbx_futures.py
@@ -1,21 +1,21 @@
-# SBX v1.2.1
+import statistics
 import time
 import ccxt
-from ta import trend, momentum
+from matplotlib import pyplot as plt
+from ta import momentum, volatility, trend
 from pandas import DataFrame as dataframe
-
-tf = '15m'
-max_leverage = 20
+import pandas_ta as pta
+tf = '5m'
+max_leverage = 5
 picker_override = None
 exclude = []
 
 exchange = ccxt.kucoinfutures({
-        'apiKey': '',
-        'secret': '',
-        'password': '',
-        'adjustForTimeDifference': True,
-    }
-)
+    'apiKey': '',
+    'secret': '',
+    'password': '',
+})
+
 
 def picker() -> str:
     markets = exchange.load_markets(True)
@@ -31,18 +31,10 @@ def picker() -> str:
             coin = i
             return coin
 
-coin = picker()
 
-def getPosition(coin:str=coin) -> dict:
-    time.sleep(exchange.rateLimit / 1000)
-    positions = exchange.fetch_positions()
-    for x in positions:
-        if x['symbol'] == coin:
-            return x
-
-def data(coin:str=coin, tf:str=tf, source:str='mark') -> dataframe:
+def Data(coin: str, tf: str = tf, source: str = 'mark') -> dataframe:
     d = {}
-    for x in ['open', 'high', 'low', 'close', 'volume']:
+    for x in ['date', 'open', 'high', 'low', 'close', 'volume']:
         d[x] = {}
     df = dataframe(exchange.fetch_ohlcv(coin, tf, params={'price': source}))
     d['volume'] = df[5]
@@ -58,43 +50,118 @@ def data(coin:str=coin, tf:str=tf, source:str='mark') -> dataframe:
     return dataframe(d)
 
 
-def takeProfits() -> None:
-    try:
-        for x in exchange.fetch_positions():
-            if x['percentage'] >= 0.02:
-                exchange.create_limit_sell_order(x['symbol'], x['contracts'], x['markPrice'], {'closeOrder':True, 'timeInForce':'GTC'})
-    except Exception as e:
-        print(e)
+def Bands(window=20, atrs=2) -> dataframe:
+    df = {}
+    atr = volatility.average_true_range(
+        data['high'], data['low'], data['close'], window)
+    df['middle'] = momentum.kama(data['close'], window)
+    df['upper'] = df['middle']+(atrs*atr)
+    df['lower'] = df['middle']-(atrs*atr)
+    return dataframe(df)
 
-print(coin)
+
+def AverageDirectionalIndex(window=28): return pta.adx(
+    data['high'], data['low'], data['close'], window)
+
+
+def b(): return float(exchange.fetch_balance()['USDT']['free'])
+def ma(window: int = 200): return trend.ema_indicator(data['close'], window)
+def ml(): return exchange.load_markets()[coin]['info']['maxLeverage']
+def lever(): return max_leverage if max_leverage < ml() else ml()
+def hi(): return Bands()['upper']
+def md(): return Bands()['middle']
+def lo(): return Bands()['lower']
+def o(): return data['open']
+def h(): return data['high']
+def l(): return data['low']
+def c(): return data['close']
+def t(): return exchange.fetch_ticker(coin)
+def q(): return (b()/c().iloc[-1])*lever()*0.1
+def bid(): return t()['info']['bestBidPrice']
+def ask(): return t()['info']['bestAskPrice']
+def adx(): return AverageDirectionalIndex()['ADX_28'].iloc[-1]
+
+
+coin = None
 while True:
-    balance = exchange.fetch_balance()['USDT']['free']
-    if coin != picker():
-        print(picker())
-        coin = picker()
-    open = data(coin)['open'].iloc[-1]
-    close = data(coin)['close'].iloc[-1]
-    maximum = exchange.load_markets()[coin]['info']['maxLeverage']
-    lever = max_leverage if max_leverage < maximum else maximum
-    qty = (balance/close) * 0.5 * lever
-    kama = momentum.kama(data()['close']).iloc[-1]
-
     try:
-        side = getPosition(coin)['side']
-    except:
-        side = None
+        if coin != picker():
+            unsettled = exchange.fetch_open_orders(coin)
+            for x in unsettled:
+                if x['info']['closeOrder'] != True:
+                    exchange.cancel_order(x['id'])
+            coin = picker()
+            print(coin)
+        data = Data(coin, tf)
 
-    try:
-        if close > kama and qty >= 1 and side != 'long':
-            print('BUY')
-            exchange.create_market_order(coin, 'buy', qty, None, {'leverage': lever})
-
-        elif close < kama and qty >= 1 and side != 'short':
-            print('SELL')
-            exchange.create_market_order(coin, 'sell', qty, None, {'leverage': lever})
-        else:
-            print('...')
-        takeProfits()
-
+        if lever() > 5:
+            print(
+                'oh F*CK! BIG DUMB IDIOT MODE IS CURRENTLY ACTIVATED!!! REDUCE LEVERAGE OR YOU WILL DIED!!!')
+        time.sleep(exchange.rateLimit/500)
+        if adx() > 20 and q() >= 1:
+            if c().iloc[-1] > ma(50).iloc[-1] > ma(200).iloc[-1]:
+                print(f'BUYING {coin}.')
+                exchange.create_market_order(
+                    coin, 'buy', q(), {'leverage': lever})
+            elif c().iloc[-1] < ma(50).iloc[-1] < ma(200).iloc[-1]:
+                print(f'SELLING {coin}.')
+                exchange.create_market_sell_order(
+                    coin, q(), {'leverage': lever})
+        time.sleep(exchange.rateLimit/500)
+        if adx() < 20 and q() >= 1:
+            if (c().iloc[-1] < lo().iloc[-1]):
+                print(f'BUYING {coin}.')
+                exchange.create_limit_buy_order(
+                    coin, q(), ask(), {'leverage': lever()})
+            elif (c().iloc[-1] > hi().iloc[-1]):
+                print(f'SELLING {coin}.')
+                exchange.create_limit_sell_order(
+                    coin, q(), bid(), {'leverage': lever()})
+        time.sleep(exchange.rateLimit/500)
+        for x in exchange.fetch_positions():
+            print(f'Checking {x["symbol"]} position status...')
+            time.sleep(exchange.rateLimit/500)
+            if x['percentage'] >= 0.02:
+                print('Placing stop...')
+                try:
+                    exchange.create_stop_limit_order(coin, 'sell' if x['side'] == 'long' else 'buy', x['contracts'], x['entryPrice'], x['entryPrice'], {
+                                                     'closeOrder': True, 'stop': 'down' if x['side'] == 'long' else 'up'})
+                except Exception:
+                    continue
+            time.sleep(exchange.rateLimit/500)
+            if x['side'] == 'long':
+                if x['markPrice'] > hi().iloc[-1] or x['percentage'] <= -0.05:
+                    print(f'CLOSING {x["symbol"]}.')
+                    exchange.create_limit_sell_order(
+                        x['symbol'], x['contracts'], x['markPrice'], {'closeOrder': True})
+            elif x['side'] == 'short':
+                if x['markPrice'] < lo().iloc[-1] or x['percentage'] <= -0.05:
+                    print(f'CLOSING {x["symbol"]}.')
+                    exchange.create_limit_buy_order(
+                        x['symbol'], x['contracts'], x['markPrice'], {'closeOrder': True})
+            time.sleep(exchange.rateLimit/500)
+            if adx() > 20 and q() >= 1:
+                print('ADX Detects a Trend...')
+                if c().iloc[-1] > ma(50).iloc[-1] > ma(200).iloc[-1]:
+                    print(f'BUYING {x["symbol"]}.')
+                    exchange.create_limit_order(
+                        x['symbol'], 'buy', q(), x['markPrice'], {'leverage': lever})
+                elif c().iloc[-1] < ma(50).iloc[-1] < ma(200).iloc[-1]:
+                    print(f'SELLING {x["symbol"]}.')
+                    exchange.create_limit_sell_order(
+                        x['symbol'], q(), x['markPrice'], {'leverage': lever})
+            elif adx() < 20 and q() >= 1:
+                print('ADX Detects a Range...')
+                if (c().iloc[-1] < lo().iloc[-1]):
+                    print(f'BUYING {x["symbol"]}.')
+                    exchange.create_limit_buy_order(
+                        x['symbol'], q(), ask(), {'leverage': lever()})
+                elif (c().iloc[-1] > hi().iloc[-1]):
+                    print(f'BUYING {x["symbol"]}.')
+                    exchange.create_limit_sell_order(
+                        x['symbol'], q(), bid(), {'leverage': lever()})
+            time.sleep(exchange.rateLimit/500)
     except Exception as e:
         print(e)
+        time.sleep(exchange.rateLimit/500)
+        continue
