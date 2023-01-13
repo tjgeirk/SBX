9012a20c4499b41bf8ab58cb643c26ee8f4fb4ae
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2023-01-13 00:00:56 -0800

    Update Trendicator.py

--- Custom_Indicators/Trendicator.py
+++ Custom_Indicators/Trendicator.py
@@ -2,10 +2,11 @@
 from ta import trend
 from pandas import DataFrame, Series
 
-def Trendicator(data:DataFrame, window:int=8, signal:int=21) -> DataFrame:
+def Trendicator(data:DataFrame, window:int=21, signal:int=8) -> DataFrame:
     sma = lambda col: trend.sma_indicator(data[col], window)
     trendicator = {}
     trendicator['upper_band'] = sma(sma('high', window), window)
-    trendicator['signal_line'] = sma(sma('close', signal), window)
+    trendicator['open_line'] = sma(sma('open', signal), window)
+    trendicator['close_line'] = sma(sma('close', signal), window)
     trendicator['lower_band'] = sma(sma('low', window), window)
     return DataFrame(trendicator)
