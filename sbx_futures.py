4146879b59e9b5ad831b28135d9d56052b015a3a
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2023-01-14 10:06:40 -0800

    Update sbx_futures.py

--- sbx_futures.py
+++ sbx_futures.py
@@ -5,35 +5,17 @@ import pandas_ta as ta
 from matplotlib import pyplot as plt
 from pandas import DataFrame as dataframe, Series as series
 
-tf = '15m'
+tf = '5m'
+max_leverage = 5
 
-max_leverage = 10
-picker_override = None
 exclude = []
 include = []
+
 exchange = ccxt.kucoinfutures({
     'apiKey': '',
     'secret': '',
     'password': '',
 })
-
-no_signal = []
-
-
-def picker() -> str:
-    z = None
-    picker = {}
-    markets = exchange.load_markets(True)
-    for v in markets:
-        if v in (no_signal or exclude):
-            continue
-        else:
-            picker[v] = markets[v]['info']['priceChgPct']
-    pick = max(picker.values())
-    for i, v in picker.items():
-        if pick == v:
-            z = i
-    return z
 
 
 def Data(coin: str, tf: str = tf) -> dataframe:
@@ -55,81 +37,90 @@ class Order:
         self.bid = float(self.t['info']['bestBidPrice'])
         self.ask = float(self.t['info']['bestAskPrice'])
         self.last = float(self.t['last'])
-        self.target_buy = self.last-(self.last*0.05)/self.lever
-        self.target_sell = self.last+(self.last*0.05)/self.lever
-        self.q = (self.b/self.last)*self.lever
+        self.q = (self.b*self.lever/self.last)/2
 
-    def sell(self) -> None:
+    def sell(self, price=None) -> None:
         print('sell')
+        target = self.last if price == None else price
         exchange.create_limit_sell_order(
-            self.coin, self.q, self.last, {'leverage': self.lever})
+            self.coin, self.q, target, {'leverage': self.lever})
 
-    def buy(self) -> None:
+    def buy(self, price=None) -> None:
         print('buy')
+        target = self.last if price == None else price
         exchange.create_limit_buy_order(
-            self.coin, self.q, self.last, {'leverage': self.lever})
+            self.coin, self.q, target, {'leverage': self.lever})
 
-    def takeProfits(self) -> None:
+
+SBX = ta.Strategy(name='SBX', ta=[
+    {'kind': 'mfi', 'length': 5},
+    {'kind': 'adx', 'length': 5},
+    {'kind': 'ema', 'close': 'close', 'length': 8},
+    {'kind': 'ema', 'close': 'close', 'length': 13},
+    {'kind': 'ema', 'close': 'close', 'length': 21},
+    {'kind': 'donchian', 'upper_length': 20, 'lower_length': 20},
+])
+
+
+while True:
+    markets = exchange.load_markets()
+    picker = {x: [markets[x]['info']['priceChgPct']] for x in markets}
+    picker = sorted(picker, key=lambda y: picker[y], reverse=True)
+    open_positions = [x['symbol'] for x in exchange.fetch_positions()]
+    coins = picker[0:5] + open_positions
+    try:
+        for coin in coins:
+            print(coin)
+            df = Data(coin, tf)
+            df.ta.strategy(SBX)
+            order = Order(coin)
+
+            if order.q >= 1:
+
+                if (ta.increasing(df['ADX_5'], 20).iloc[-1] == 1 and
+                    df['DCU_20_20'].iloc[-1] > df['DCU_20_20'].iloc[-2] and
+                    df['MFI_5'].iloc[-1] > 50 and
+                    df['EMA_8'].iloc[-1] > df['EMA_13'].iloc[-1] > df['EMA_21'].iloc[-1]
+                    ):
+                    order.buy()
+
+                if (ta.increasing(df['ADX_5'], 20).iloc[-1] == 1 and
+                    df['DCL_20_20'].iloc[-1] < df['DCL_20_20'].iloc[-2] and
+                    df['MFI_5'].iloc[-1] < 50 and
+                    df['EMA_8'].iloc[-1] < df['EMA_13'].iloc[-1] < df['EMA_21'].iloc[-1]
+                    ):
+                    order.sell()
+
         for x in exchange.fetch_positions():
-            if x['percentage'] >= 0.05 or x['percentage'] <= -0.25:
-                exchange.create_stop_limit_order(self.coin, 'sell' if x['side'] == 'long' else 'buy', x['contracts'], x['markPrice'], x['entryPrice'], {
-                                                 'closeOrder': True, 'stop': 'down' if x['side'] == 'long' else 'up', 'remark': 'Percentage'})
+            order = Order(x['symbol'])
+            df = Data(x['symbol'], tf)
+            df.ta.strategy(SBX)
+
+            if (x['side'] == 'long' and 
+                ta.decreasing(df['DMP_5'], 20).iloc[-1] == 1 and ta.decreasing(df['MFI_5'], 20).iloc[-1] == 1):
+                if x['percentage'] > 0:
+                    order.sell(x['markPrice'])
+                else:
+                    order.sell()
+
+            if (x['side'] == 'short' and 
+                ta.decreasing(df['DMN_5'], 20).iloc[-1] == 1 and ta.increasing(df['MFI_5'], 20).iloc[-1] == 1):
+                if x['percentage'] > 0:
+                    order.buy(x['markPrice'])
+                else:
+                    order.buy()
+
+            if (x['percentage'] >= 0.05 or x['percentage'] <= -0.1):
+                (lambda: exchange.create_stop_limit_order(x['symbol'], 'sell' if x['side'] == 'long' else 'buy', x['contracts'], x['markPrice'], x['markPrice'], {
+                 'closeOrder': True, 'stop': 'down' if x['side'] == 'long' else 'up',  'reduceOnly': True}))()
 
         open_orders = sorted(exchange.fetch_open_orders(),
                              key=lambda x: x['timestamp'], reverse=True)
-
         for i, x in enumerate(open_orders):
-            if i in range(0, 6):
+            if i in range(0, 6) or x['info']['closeOrder'] == True:
                 continue
             else:
                 exchange.cancel_order(x['id'])
-
-
-no_signal = []
-
-
-def dropCoin(coin):
-    global no_signal
-    print(f'Dropping {coin} from trade pairs list.')
-    no_signal.append(coin)
-    if len(no_signal) >= 10:
-        no_signal = []
-
-
-SBX = ta.Strategy(name="SBX", ta=[
-    {"kind": "ha"},
-    {"kind": "ema", "close": "HA_close", "length": 20},
-    {"kind": "ema", "close": "HA_close", "length": 50},
-    {"kind": "ema", "close": "HA_close", "length": 200},
-    {"kind": "adx"},
-    {"kind": "donchian"},
-])
-
-while True:
-    try:
-        coins = [x['symbol'] for x in exchange.fetch_positions()]
-        if picker() not in coins or exclude:
-            coins.append(picker())
-        for coin in list(set(coins)):
-            print(coin)
-            df = Data(coin, tf)
-            order = Order(coin)
-
-            df.ta.strategy(SBX)
-
-            if order.q >= 1:
-                if df['EMA_20'].iloc[-1] > df['EMA_50'].iloc[-1] > df['EMA_200'].iloc[-1] and df['HA_open'].iloc[-1] < df['HA_close'].iloc[-1]:
-                    order.buy()
-                if df['EMA_20'].iloc[-1] < df['EMA_50'].iloc[-1] < df['EMA_200'].iloc[-1] and df['HA_open'].iloc[-1] > df['HA_close'].iloc[-1]:
-                    order.sell()
-                else:
-                    dropCoin(coin)
-            else:
-                for x in exchange.fetch_open_orders():
-                    if x['info']['closeOrder'] == False:
-                        exchange.cancel_order(x['id'])
-
-            order.takeProfits()
 
     except Exception as e:
         print(e)
