f335877746e1a3a8c4b1940d99501cea82e8ab36
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2023-02-06 17:58:30 -0800

    Update sbx_futures.py

--- sbx_futures.py
+++ sbx_futures.py
@@ -1,182 +1,188 @@
+import concurrent.futures
+import ccxt
 import pandas as pd
-import ccxt.kucoinfutures as kucoinfutures
 import pandas_ta as ta
+import numpy as np
 import time
+import openai
 
 tf = '5m'
 max_leverage = 5
 take_profit = 0.01
 stop_loss = 0.2
-martingale = 0.01
+martingale = 0.02
 
-exchange = kucoinfutures({
+exchange = ccxt.kucoinfutures({
     'apiKey': '',
     'secret': '',
     'password': '',
+    'adjustForTimeDifference': True,
 })
 
-def Data(coin, tf=tf):
-    data = {}
-    ohlcv = exchange.fetch_ohlcv(coin, tf, limit=1000)
-    data['date'] = [x[0] for x in ohlcv]
-    data['open'] = [x[1] for x in ohlcv]
-    data['high'] = [x[2] for x in ohlcv]
-    data['low'] = [x[3] for x in ohlcv]
-    data['close'] = [x[4] for x in ohlcv]
-    data['volume'] = [x[5] for x in ohlcv]
-    return pd.DataFrame(data)
+# openai.api_key = ''
+
+
+def Data(coin, tf=tf) -> pd.DataFrame:
+    ohlcv = exchange.fetch_ohlcv(coin, tf, limit=500)
+    columns = ['dates', 'opens', 'highs', 'lows', 'closes', 'volumes']
+    df = pd.DataFrame({})
+    for i, v in enumerate(columns):
+        df[v] = np.array([x[i] for x in ohlcv],
+                         dtype=np.float64 if i > 0 else np.int64)
+    return pd.DataFrame(df)
+
 
 class Order:
-    def __init__(self, coin):
+    def __init__(self, coin: str) -> None:
         self.coin = coin
         self.balance = float(exchange.fetch_balance()['USDT']['total'])
-        self.lever = exchange.load_markets()[coin]['info']['maxLeverage']
-        self.lever = min(max_leverage, self.lever)
-        self.last = float(exchange.fetch_ticker(coin)['last'])
-        self.q = float(self.balance/self.last)*0.01
-        self.q = 1 if self.q < 1 else self.q
+        self.lever = min(max_leverage, exchange.load_markets(
+        )[self.coin]['info']['maxLeverage'])
+        self.last = float(exchange.fetch_ticker(self.coin)['last'])
+        self.q = max([float(self.balance/self.last)*0.01, 1])
 
-    def sell(self, price=None, side=None, qty=None):
+    def sell(self, price=None, side=None, qty=None) -> None:
         print('sell', self.coin)
-        q = self.q if qty is None else qty
-        target = self.last if price is None else price
-        exchange.create_limit_sell_order(
-            self.coin, q, target, {'leverage': self.lever, 'closeOrder': True if side == 'long' else False})
-        exchange.create_stop_limit_order(
-            self.coin, 'buy', self.q, (target-(target*0.1/self.lever)), (target-(target*0.1/self.lever)), {'closeOrder': True})
+        q = self.q if qty == None else qty
+        target = self.last if price == None else price
 
-    def buy(self, price=None, side=None, qty=None):
+        try:
+            exchange.create_limit_sell_order(self.coin, q, target, {
+                'leverage': self.lever, 'closeOrder': True if side == 'long' else False})
+        except ccxt.BaseError as e:
+            print('Issue Selling!', e)
+
+        try:
+            exchange.create_stop_limit_order(self.coin, 'buy', q, (target-(
+                target*0.1/self.lever)), (target-(target*0.1/self.lever)), {'closeOrder': True})
+        except ccxt.BaseError as e:
+            print('Issue Placing Stop!', e)
+
+    def buy(self, price=None, side=None, qty=None) -> None:
         print('buy', self.coin)
-        q = self.q if qty is None else qty
-        target = self.last if price is None else price
-        exchange.create_limit_buy_order(
-            self.coin, q, target, {'leverage': self.lever, 'closeOrder': True if side == 'short' else False})
-        exchange.create_stop_limit_order(
-            self.coin, 'sell', self.q, (target+(target*0.1/self.lever)), (target+(target*0.1/self.lever)),
+        q = self.q if qty == None else qty
+        target = self.last if price == None else price
 
-            
+        try:
+            exchange.create_limit_buy_order(self.coin, q, target, {
+                'leverage': self.lever, 'closeOrder': True if side == 'short' else False})
+        except ccxt.BaseError as e:
+            print('Issue Buying!', e)
+
+        try:
+            exchange.create_stop_limit_order(self.coin, 'sell', q, (target+(
+                target*0.1/self.lever)), (target+(target*0.1/self.lever)), {'closeOrder': True})
+        except ccxt.BaseError as e:
+            print('Issue Placing Stop!', e)
+
+
 SBX = ta.Strategy(name='SBX', ta=[
     {'kind': 'ha'},
-    {'kind': 'mfi', 'length': 2},
-    {'kind': 'adx', 'length': 3},
-    {'kind': 'ema', 'close': 'close', 'length': 8},
-    {'kind': 'ema', 'close': 'close', 'length': 13},
-    {'kind': 'ema', 'close': 'close', 'length': 21},
-    {'kind': 'vwma', 'close': 'close', 'length': 50},
-    {'kind': 'vwma', 'close': 'close', 'length': 200},
+    {'kind': 'ema', 'close': 'close', 'length': 2},
+    {'kind': 'ema', 'close': 'close', 'length': 3},
+    {'kind': 'ema', 'close': 'close', 'length': 4},
+    {'kind': 'ema', 'close': 'close', 'length': 200},
 ])
 
 
-def strategy(coin: str, tf: str) -> None:
-    try:
-        sleep(exchange.rateLimit/1000)
-        df = Data(coin, tf)
-        order = Order(coin)
-        df.ta.strategy(SBX)
+def process_coin(coin: str) -> None:
+    df = Data(coin, tf)
+    order = Order(coin)
+    df.ta.strategy(SBX)
 
-        if (
-            df['EMA_8'].iloc[-1] > df['EMA_13'].iloc[-1] > df['EMA_21'].iloc[-1] and
-            df['HA_close'].iloc[-1] > df['VWMA_50'].iloc[-1] > df['VWMA_200'].iloc[-1] and
-            df['DMP_3'].iloc[-1] > df['DMN_3'].iloc[-1] and
-            df['MFI_2'].iloc[-1] >= 50 and
-            df['HA_close'].iloc[-1] > df['HA_open'].iloc[-1]
-        ):
-            order.buy()
+    ema2 = df['EMA_2'].iloc[-1]
+    ema3 = df['EMA_3'].iloc[-1]
+    ema5 = df['EMA_5'].iloc[-1]
+    ema200 = df['EMA_200'].iloc[-1]
+    ha_close = df['HA_close'].iloc[-1]
+    ha_open = df['HA_open'].iloc[-1]
+    
+    # input_text = ''
+    # input_text += f"Closes: {df['HA_close']}, Opens: {df['HA_open']}"
 
-        elif (
-            df['EMA_8'].iloc[-1] < df['EMA_13'].iloc[-1] < df['EMA_21'].iloc[-1] and
-            df['HA_close'].iloc[-1] < df['VWMA_50'].iloc[-1] < df['VWMA_200'].iloc[-1] and
-            df['DMP_3'].iloc[-1] < df['DMN_3'].iloc[-1] and
-            df['MFI_2'].iloc[-1] <= 50 and
-            df['HA_close'].iloc[-1] < df['HA_open'].iloc[-1]
-        ):
-            order.sell()
-    except Exception as e:
-        print(e)
-        pass
+    # prompt = (f"Please identify the average number of periods between trend reversals based on these haikin-ashi candles, and return only the number with no additional text:\n\n"
+    #           f"{input_text}")
+    
+    # completions = openai.Completion.create(
+    #     engine="text-davinci-002",
+    #     prompt=prompt,
+    #     max_tokens=1024,
+    #     n=1,
+    #     stop=None,
+    #     temperature=0.5,
+    # )
+    # interval = completions.choices[0].text.strip()
+
+    # print(interval)
+
+    if (ema2 > ema3 > ema5 > ema200) and (ha_close > ha_open):
+        order.buy()
+    if (ema2 < ema3 < ema5 < ema200) and (ha_close < ha_open):
+        order.sell()
 
 
-def martingale(x: dataframe, tf: str) -> None:
-    try:
-        sleep(exchange.rateLimit/1000)
-        df = Data(x['symbol'], tf)
-        order = Order(x['symbol'])
-        df.ta.strategy(SBX)
 
-        if x['side'] == 'long':
 
-            if df['HA_close'].iloc[-1] < df['VWMA_50'].iloc[-1] and x['percentage'] > 0:
-                order.sell(x['markPrice'], x['side'], x['contracts'])
+def process_position(x) -> None:
+    order = Order(x['symbol'])
+    coin = x['symbol']
+    if x['side'] == 'long':
+        if x['percentage'] <= -abs(martingale):
+            order.buy(x['markPrice'], x['side'], x['contracts'])
+        if x['percentage'] <= -abs(stop_loss):
+            order.sell(x['markPrice'], x['side'], x['contracts'])
+        if x['percentage'] >= abs(take_profit):
+            order.sell(x['markPrice'], x['side'], x['contracts'])
 
-            if df['HA_close'].iloc[-1] < df['HA_open'].iloc[-1] and x['percentage'] > 0:
-                order.sell(x['markPrice'], x['side'], x['contracts'])
+    elif x['side'] == 'short':
+        if x['percentage'] <= -abs(martingale):
+            order.sell(x['markPrice'], x['side'], x['contracts'])
+        if x['percentage'] <= -abs(stop_loss):
+            order.buy(x['markPrice'], x['side'], x['contracts'])
+        if x['percentage'] >= abs(take_profit):
+            order.buy(x['markPrice'], x['side'], x['contracts'])
 
-            if x['percentage'] <= -abs(martingale_pcnt):
-                order.buy(x['markPrice'], x['side'])
 
-            if x['percentage'] <= -abs(stop_loss):
-                order.sell(x['markPrice'], x['side'], x['contracts'])
-
-            if x['percentage'] >= abs(take_profit):
-                order.sell(x['markPrice'], x['side'], x['contracts'])
-
-        elif x['side'] == 'short':
-
-            if df['HA_close'].iloc[-1] > df['VWMA_50'].iloc[-1] and x['percentage'] > 0:
-                order.buy(x['markPrice'], x['side'], x['contracts'])
-
-            if df['HA_close'].iloc[-1] > df['HA_open'].iloc[-1] and x['percentage'] > 0:
-                order.buy(x['markPrice'], x['side'], x['contracts'])
-
-            if x['percentage'] <= -abs(martingale_pcnt):
-                order.sell(x['markPrice'], x['side'])
-
-            if x['percentage'] <= -abs(stop_loss):
-                order.buy(x['markPrice'], x['side'], x['contracts'])
-
-            if x['percentage'] >= abs(take_profit):
-                order.buy(x['markPrice'], x['side'], x['contracts'])
-
-    except Exception as e:
-        print(e)
-        pass
-
+def cancelOrders() -> None:
+    orders = exchange.fetch_open_orders()
+    most_recent_orders = {}
+    for order in orders:
+        symbol = order['symbol']
+        timestamp = order['timestamp']
+        if symbol not in most_recent_orders:
+            most_recent_orders[symbol] = (timestamp, order['id'])
+        else:
+            prev_timestamp, prev_order_id = most_recent_orders[symbol]
+            if timestamp > prev_timestamp:
+                exchange.cancel_order(prev_order_id)
+                most_recent_orders[symbol] = (timestamp, order['id'])
+            else:
+                exchange.cancel_order(order['id'])
 
 while True:
     try:
-        sleep(exchange.rateLimit/1000)
-        markets = exchange.load_markets()
-        picker = {x: [markets[x]['info']['priceChgPct']] for x in markets}
-        picker = sorted(picker, key=lambda y: picker[y], reverse=True)
-        positions = [x['symbol'] for x in exchange.fetch_positions()]
-        coins = picker[0:max_new_positions] + positions
+        time.sleep(exchange.rateLimit/1000)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            markets = exchange.load_markets()
+            picker = sorted({x: [markets[x]['info']['priceChgPct']]
+                            for x in markets}, key=lambda y: y[1], reverse=True)
+            positions = [x['symbol'] for x in exchange.fetch_positions()]
+            coins = picker[0:4] + positions
 
-        if exchange.fetch_balance()['USDT']['free'] > exchange.fetch_balance()['USDT']['total']/2:
+            strat = {executor.submit(process_coin, coin)
+                                     : coin for coin in coins}
 
-            for coin in coins:
-                print(coin)
-                strategy(coin, tf)
+            pos = {executor.submit(process_position, x)
+                                   : x for x in exchange.fetch_positions()}
 
-        for x in exchange.fetch_positions():
-            print(x['symbol'])
-            martingale(x, tf)
+            for future in concurrent.futures.as_completed(strat):
+                continue
+
+            for future in concurrent.futures.as_completed(pos):
+                continue
+
+            cancelOrders()
 
     except Exception as e:
         print(e)
-        sleep(exchange.rateLimit/1000)
-        queue = {x['symbol'] for x in exchange.fetch_open_orders()}
-        for y in [x for x in queue if x not in coins]:
-            queue.remove(y)
-        for x in queue:
-            y = sorted(exchange.fetch_open_orders(
-                x), key=lambda x: x['timestamp'])
-            for a, b in enumerate(y):
-                if a in range(0, 6):
-                    continue
-                else:
-                    try:
-                        exchange.cancel_order(b['id'])
-                    except Exception as e:
-                        print(e)
-                        continue
         continue
