9430c59f7c263014804109a17e8ed3132503afd9
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2023-02-19 07:31:26 -0800

    Update Bollinger_Aint_Dead.py

--- Successful Bots/Bollinger_Aint_Dead.py
+++ Successful Bots/Bollinger_Aint_Dead.py
@@ -1,3 +1,10 @@
+## "I'm John Bollinger. I'm the guy who created
+##  Bollinger Bands, and I am not dead." 
+##                        -John Bollinger, 2019
+##
+##
+
+
 import ta.volatility as vol
 import pandas as pd
 import ccxt.async_support as ccxt
